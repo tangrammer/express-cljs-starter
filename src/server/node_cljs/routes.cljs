82136c97a0cdeb2ab39a58299e1c79e135d765c7
Juan A. Ruz <juanantonioruz@gmail.com>
2016-04-06 17:22:32 +0200

    WIP #2 Following juxt/yada interceptor chain approach and data oriented resource definition

--- src/server/node_cljs/routes.cljs
+++ src/server/node_cljs/routes.cljs
@@ -2,13 +2,15 @@
   (:require [node-cljs.express :refer [app]]
             [node-cljs.log :as log]))
 
+(declare GET)
+
 (defn path-info [req res]
   (log/debug "path-info controller")
   (.json res #js {"path" (.-path req)}))
 
 (defn default-route [req res]
   (log/debug "default-route controller")
-  (.send res (.-path req)))
+  (.send res (str "default-route::" (.-path req))))
 
 (.use app
   (fn [req res next]
@@ -17,8 +19,24 @@
 
 (.get app "/" #(.send %2 "home"))
 
+(defn defroute [a b fn-controller]
+  (condp = a
+    GET (.get app b (fn [req res]
+                      (let [req* {:params (js->clj (.-params req) :keywordize-keys true)}
+                            r (fn-controller req* res)]
+                        (.json res r))))))
+
+(defroute GET "/v1/customer/:customerId"
+  (fn [req res]
+    #js {:customer (-> req :params :customerId)}))
+
+
+
+
+
 (.get app "/1" path-info)
 (.get app "/2" path-info)
 (.get app "/3" path-info)
+
 
 (.use app default-route)
