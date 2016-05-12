411778013659fa4dd3b6faa636148594609f4bbe
Juan A. Ruz <juanantonioruz@gmail.com>
2016-05-12 11:25:19 +0200

    Use parsed json version as mocks instead of parsing string each time

--- src/main/clojure/rebujito/store/mocks.clj
+++ src/main/clojure/rebujito/store/mocks.clj
@@ -2,61 +2,55 @@
   (:require [cheshire.core :as json]
             [clojure.string :refer (split)]))
 
-(def card (json/parse-string "{
-   \"cardCurrency\":\"USD\",
-   \"cardId\":\"85607AF09DD610\",
-   \"cardNumber\":\"7777064199346273\",
-   \"nickname\":\"My Card (6273)\",
-   \"class\":\"242\",
-   \"type\":\"Standard\",
-   \"balanceCurrencyCode\":\"USD\",
-   \"submarketCode\":\"US\",
-   \"balance\":0.00,
-   \"balanceDate\":\"2014-03-07T19:52:42.0500000Z\",
-   \"imageUrls\":[{
-         \"imageType\":\"ImageIcon\",
-         \"uri\":\"http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_88.png\"},
-      {
-         \"imageType\":\"ImageSmall\",
-         \"uri\":\"http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_115.png\"
-               },
-      {
-         \"imageType\":\"ImageMedium\",
-         \"uri\":\"http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_165.png\"
-               },
-      {
-         \"imageType\":\"ImageLarge\",
-         \"uri\":\"http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_261.png\"
-               },
-      {
-         \"imageType\":\"iosThumb\",
-         \"uri\":\"http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_thumb_82.png\"
-               },
-      {
-         \"imageType\":\"iosThumbHighRes\",
-         \"uri\":\"http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_thumb_82@2x.png\"
-               },
-      {
-         \"imageType\":\"iosLarge\",
-         \"uri\":\"http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_270.png\"
-               },
-      {
-         \"imageType\":\"iosLargeHighRes\",
-         \"uri\":\"http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_270@2x.png\"
-               }
-   ],
-   \"primary\":false,
-   \"partner\":false,
-   \"autoReloadProfile\":null,
-   \"digital\":true,
-   \"owner\":true,
-   \"actions\":[
-      \"AutoReload\",
-      \"Transfer\",
-      \"Reload\"
-   ]
-  }"
-                             true))
+
+(def mimi-card {:id "7777064199346273"
+                :currency "USD"
+                :balance 0.0
+                :balanceDate "2014-03-07T19:52:42.0500000Z"})
+
+(def card {:cardNumber "7777064199346273"
+           :primary false
+           :cardCurrency "USD"
+           :partner false
+           :nickname "My Card (6273)"
+           :type "Standard"
+           :actions ["AutoReload" "Transfer" "Reload"]
+           :imageUrls
+           [
+            {:imageType "ImageIcon"
+             :uri
+             "http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_88.png"}
+            {:imageType "ImageSmall"
+             :uri
+             "http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_115.png"}
+            {:imageType "ImageMedium"
+             :uri
+             "http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_165.png"}
+            {:imageType "ImageLarge"
+             :uri
+             "http://globalassets.starbucks.com/images/cardimages/card_Coffee_Aroma_Card_FY11_261.png"}
+            {:imageType "iosThumb"
+             :uri
+             "http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_thumb_82.png"}
+            {:imageType "iosThumbHighRes"
+             :uri
+             "http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_thumb_82@2x.png"}
+            {:imageType "iosLarge"
+             :uri
+             "http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_270.png"}
+            {:imageType "iosLargeHighRes"
+             :uri
+             "http://starbucks.com/images/mobilev3/card_Coffee_Aroma_Card_FY11_270@2x.png"}]
+           :balance 0.0
+           :submarketCode "US"
+           :cardId "85607AF09DD610"
+           :class "242"
+           :balanceDate "2014-03-07T19:52:42.0500000Z"
+           :digital true
+           :owner true
+           :autoReloadProfile nil
+           :balanceCurrencyCode "USD"})
+
 
 (def get-payment-method-detail (json/parse-string "{
    \"isTemporary\":false,
