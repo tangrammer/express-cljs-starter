a2c1d3dbb771665f9d0d877773f41cd84a8fc84f
Marcin Jekot <marcin@jekot.net>
2016-05-14 07:43:48 +0200

    parse some more fields

--- mimi/src/mimi/routes.cljs
+++ mimi/src/mimi/routes.cljs
@@ -9,10 +9,33 @@
 
 (.get app "/mimi/health" #(.send %2 "ok"))
 
+(defn parse-customer-fields [req-body]
+  (let [firstname (get req-body "firstname")
+        lastname (get req-body "lastname")
+        password (get req-body "password")
+        email (get req-body "email")
+        mobile (get req-body "mobile")
+        address (get req-body "address")
+        city (get req-body "city")
+        region (get req-body "region")
+        country (get req-body "country")
+        postalcode (get req-body "postalcode")]
+    {:firstname firstname
+     :lastname lastname
+     :password password
+     :email email
+     :mobile mobile
+     :address address
+     :city city
+     :region region
+     :country country
+     :postalcode postalcode}))
+
 (.post app "/mimi/starbucks/account"
   (fn
     [req res]
     "create a starbucks customer in micros"
-    (let [body (.-body req)]
-      (log/debug "gots" body)
-      (.send res "oks"))))
+    (let [payload (parse-customer-fields (->> req .-body js->clj))]
+      ; (log/debug "got" payload)
+      (prn payload)
+      (.send res "ok"))))
