9d0b8597c22cb46ca8ca0e6c9508564700b11b8d
Juan A. Ruz <juanantonioruz@gmail.com>
2016-05-18 15:27:31 +0200

    Add rebujito.coercion as utils about schema coercion

---
+++ src/main/clojure/rebujito/coercion.clj
@@ -0,0 +1,15 @@
+;; utils about schema coercion
+(ns rebujito.coercion)
+
+
+(defn throw-coercer-exceptions
+  "wraps a coercer so it throws an error in error case.
+By default p/schema catch all errors "
+  [c]
+  (fn [schema]
+    (let [coercion-result (c schema)]
+      (if (= schema.utils.ErrorContainer (type coercion-result))
+        (->
+         (str "PARSE_ID_ERROR: " (schema.utils/validation-error-explain (->  coercion-result :error)))
+         schema.macros/error!)
+        coercion-result))))
