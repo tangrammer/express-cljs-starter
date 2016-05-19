df7742145d089265e6d40c729862da7fea1c42ba
Juan A. Ruz <juanantonioruz@gmail.com>
2016-05-20 00:24:32 +0200

    Add async support to account resource to not block on http-calls in this case for communicate with mimi http services

--- src/main/clojure/rebujito/api/resources/account.clj
+++ src/main/clojure/rebujito/api/resources/account.clj
@@ -1,11 +1,11 @@
 (ns rebujito.api.resources.account
   (:require
+   [manifold.deferred :as d]
    [rebujito.api.util :refer :all]
    [rebujito.mimi :as mim]
    [rebujito.protocols :as p]
    [schema.core :as s]
-   [yada.resource :refer [resource]]
-   ))
+   [yada.resource :refer [resource]]))
 
 (def schema {:post {
                     :addressLine1 String
@@ -51,30 +51,33 @@
                 :consumes [{:media-type #{"application/json"}
                             :charset "UTF-8"}]
                 :response (fn [ctx]
-                            (let [[code res] (p/create-account mimi (get-in ctx [:parameters :body]))]
-                              (condp =  code
-                                      "111000" (>400 ctx ["Username is already taken" "Account Management Service returns error that user name is already taken"])
-                                      "111001" (>400 ctx ["Unknown error occured" "Account Management Service returns error
+                            (->
+                             (let [[code res] (p/create-account mimi (get-in ctx [:parameters :body]))]
+                               (condp =  code
+                                 "111000" (>400 ctx ["Username is already taken" "Account Management Service returns error that user name is already taken"])
+                                 "111001" (>400 ctx ["Unknown error occured" "Account Management Service returns error
 "])
-                                      "111005" (>400 ctx ["Email address too long. Must be 50 characters or less." ""])
-                                      "111008" (>400 ctx ["Please supply an email address" "Missing emailAddress attribute."])
-                                      "111009" (>400 ctx ["Please supply a registration source" "Missing registration source attribute."])
-                                      "111011" (>400 ctx ["Please supply a password" "Missing password attribute"])
-                                      "111012" (>400 ctx ["Please supply a market" "Missing market parameter is required"])
-                                      "111015" (>400 ctx ["Please supply a last name" "Missing lastName attribute"])
-                                      "111016" (>400 ctx ["Please supply a first name" "Missing firstName attribute"])
-                                      "111022" (>400 ctx ["Password does not meet complexity requirements" "Account Management Service returns error that password does not meet complexity requirements"])
-                                      "111023" (>400 ctx ["No Request supplied" "Create Account Request was malformed."])
-                                      "111025" (>400 ctx ["Invalid postalCode. No results were resolved from geolookup" "Return this message when no results are found when resolving address from postal code when market = US"])
-                                      "111027" (>400 ctx ["Email address is not unique" "Account Management Service returns error that email address is already taken"])
-                                      "111036" (>400 ctx ["Invalid characters (Ø, ø, Ë, ë) specified for first and/or last name." ""])
-                                      "111039" (>400 ctx ["Invalid market code or country code." ""])
-                                      "111041" (>400 ctx ["Invalid email address" "Email address was malformed"])
-                                      "111046" (>400 ctx ["firstName failed profanity check." ""])
-                                      "500" (>500 ctx ["An unexpected error occurred processing the request."])
-                                      (>201 ctx (p/get-and-insert! user-store (assoc (get-in ctx [:parameters :body])
-                                                                                     :_id (p/generate-id user-store (first res))))
-                                            ))))}}}
+                                 "111005" (>400 ctx ["Email address too long. Must be 50 characters or less." ""])
+                                 "111008" (>400 ctx ["Please supply an email address" "Missing emailAddress attribute."])
+                                 "111009" (>400 ctx ["Please supply a registration source" "Missing registration source attribute."])
+                                 "111011" (>400 ctx ["Please supply a password" "Missing password attribute"])
+                                 "111012" (>400 ctx ["Please supply a market" "Missing market parameter is required"])
+                                 "111015" (>400 ctx ["Please supply a last name" "Missing lastName attribute"])
+                                 "111016" (>400 ctx ["Please supply a first name" "Missing firstName attribute"])
+                                 "111022" (>400 ctx ["Password does not meet complexity requirements" "Account Management Service returns error that password does not meet complexity requirements"])
+                                 "111023" (>400 ctx ["No Request supplied" "Create Account Request was malformed."])
+                                 "111025" (>400 ctx ["Invalid postalCode. No results were resolved from geolookup" "Return this message when no results are found when resolving address from postal code when market = US"])
+                                 "111027" (>400 ctx ["Email address is not unique" "Account Management Service returns error that email address is already taken"])
+                                 "111036" (>400 ctx ["Invalid characters (Ø, ø, Ë, ë) specified for first and/or last name." ""])
+                                 "111039" (>400 ctx ["Invalid market code or country code." ""])
+                                 "111041" (>400 ctx ["Invalid email address" "Email address was malformed"])
+                                 "111046" (>400 ctx ["firstName failed profanity check." ""])
+                                 "500" (>500 ctx ["An unexpected error occurred processing the request."])
+                                 (>201 ctx (p/get-and-insert! user-store (assoc (get-in ctx [:parameters :body])
+                                                                                :_id (p/generate-id user-store (first res))))
+                                       )))
+                             d/future
+                             ))}}}
 
 
        (merge (common-resource :account))
