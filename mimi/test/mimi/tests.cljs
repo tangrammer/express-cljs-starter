6f3409d578016bcfc60379cd1581baadf2596527
Marcin Jekot <marcin@jekot.net>
2016-05-23 13:34:55 +0200

    remove password mobile and gender from mimi

--- mimi/test/mimi/tests.cljs
+++ mimi/test/mimi/tests.cljs
@@ -7,13 +7,10 @@
 (def valid-create-customer-data
   {:firstname "juan"
    :lastname "dos"
-   :password "pa"
    :email "123"
-   :mobile "123"
    :city "123"
    :region "123"
    :postalcode "123"
-   :gender "male"
    :birth {:month 5
            :dayOfMonth 31}})
 
@@ -21,8 +18,8 @@
   (is (nil? (validate-create-customer-data valid-create-customer-data)))
   (is (not (nil? (validate-create-customer-data {:arb :data}))))
   (is (not (nil? (validate-create-customer-data (assoc valid-create-customer-data :extra :field)))))
-  (is (not (nil? (validate-create-customer-data (dissoc valid-create-customer-data :gender)))))
-  (is (not (nil? (validate-create-customer-data (assoc (dissoc valid-create-customer-data :gender) :extra :field))))))
+  (is (not (nil? (validate-create-customer-data (dissoc valid-create-customer-data :email)))))
+  (is (not (nil? (validate-create-customer-data (assoc (dissoc valid-create-customer-data :email) :extra :field))))))
 
 (nodejs/enable-util-print!)
 (print "\n\nrunning tests...")
