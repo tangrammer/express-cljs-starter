dac41d572f51ff97d9e53e647e58393d52249137
Marcin Jekot <marcin@jekot.net>
2016-05-23 19:26:35 +0200

    make export more idempotent: remove records before inserting them

--- export/export.js
+++ export/export.js
@@ -11,8 +11,8 @@ const
   username = 'SCV_SWARM',
   password = '$35L$3a9w'
 
-// const accountNumber = '9623570800021'
-const accountNumber = '9623570800136'
+const accountNumber = '9623570800021'
+// const accountNumber = '9623570800136'
 
 const cols = [
   'firstname',
@@ -30,6 +30,7 @@ Promise.all([
 ])
 
 .then(([[customer], txs]) => {
+  console.log(customer.id)
   return exportCustomer(customer)
   .then(() => {
     return exportTransactions(customer, txs)
@@ -43,36 +44,44 @@ function exportCustomer(customer) {
 
   console.log(`exporting ${customer.primaryposref}`)
 
-  const ps = new sql.PreparedStatement()
+  console.log(customer.id)
 
-  return new Promise((resolve, reject) => {
+  return sql.query`delete from customers where id = ${customer.id}`
+  .then(() => {
 
-    ps.input('id', sql.Int)
-    ps.input('primaryposref', sql.VarChar(128))
-    ps.input('first_name', sql.VarChar(128))
-    ps.input('last_name', sql.VarChar(128))
-    ps.input('email', sql.VarChar(128))
-    ps.input('city', sql.VarChar(128))
-    ps.input('province', sql.VarChar(128))
+    const ps = new sql.PreparedStatement()
 
-    ps.prepare('insert into customers(id, primaryposref, first_name, last_name, email, city, province) values(@id, @primaryposref, @first_name, @last_name, @email, @city, @province)', err => {
+    return new Promise((resolve, reject) => {
 
-      if (err) throw err
+      ps.input('id', sql.Int)
+      ps.input('primaryposref', sql.VarChar(128))
+      ps.input('first_name', sql.VarChar(128))
+      ps.input('last_name', sql.VarChar(128))
+      ps.input('email', sql.VarChar(128))
+      ps.input('city', sql.VarChar(128))
+      ps.input('province', sql.VarChar(128))
 
-      ps.execute({
-        id: customer.id,
-        primaryposref: customer.primaryposref,
-        'first_name': customer.firstname,
-        'last_name': customer.lastname,
-        email: customer.email,
-        city: customer.city,
-        province: customer.region,
-      }, (err, recordsets, affected) => {
+      ps.prepare('insert into customers(id, primaryposref, first_name, last_name, email, city, province) values(@id, @primaryposref, @first_name, @last_name, @email, @city, @province)', err => {
 
-        console.log('inserted customer')
+        if (err) throw err
 
-        ps.unprepare(err => {
-          resolve()
+        ps.execute({
+          id: customer.id,
+          primaryposref: customer.primaryposref,
+          'first_name': customer.firstname,
+          'last_name': customer.lastname,
+          email: customer.email,
+          city: customer.city,
+          province: customer.region,
+        }, (err, recordsets, affected) => {
+
+          if (err) return reject(err)
+
+          console.log(`inserted customer ${customer.primaryposref}`)
+
+          ps.unprepare(err => {
+            resolve()
+          })
         })
       })
     })
@@ -80,27 +89,31 @@ function exportCustomer(customer) {
 }
 
 function exportTransactions(customer, txs) {
-  var table = new sql.Table('transactions')
 
-  table.create = true
+  console.log(`exporting transactions for ${customer.primaryposref}`)
 
-  console.log('customer', customer.id)
+  return removeTransactions(customer)
+  .then(() => {
+    const table = new sql.Table('transactions')
+    table.create = true
 
-  table.columns.add('id', sql.Int, {nullable: false})
-  table.columns.add('customer_id', sql.Int, {nullable: false})
-  table.columns.add('check_number', sql.Int, {nullable: false})
-  table.columns.add('amount', sql.Decimal(19, 2), {nullable: false})
-  table.columns.add('balance', sql.Decimal(19, 2), {nullable: false})
-  table.columns.add('description', sql.VarChar(128), {nullable: false})
+    table.columns.add('id', sql.Int, {nullable: false})
+    table.columns.add('customer_id', sql.Int, {nullable: false})
+    table.columns.add('check_number', sql.Int, {nullable: false})
+    table.columns.add('amount', sql.Decimal(19, 2), {nullable: false})
+    table.columns.add('balance', sql.Decimal(19, 2), {nullable: false})
+    table.columns.add('description', sql.VarChar(128), {nullable: false})
 
-  txs.forEach((tx) => {
-    table.rows.add(tx.id, customer.id, tx.check, tx.amount, tx.balance, tx.description)
-    exportCheckItems(tx, tx.items)
-  })
+    txs.forEach((tx) => {
+      table.rows.add(tx.id, customer.id, tx.check, tx.amount, tx.balance, tx.description)
+      exportCheckItems(tx, tx.items)
+    })
 
-  var request = new sql.Request()
-  request.bulk(table, function(err, rowCount) {
-    console.log('done, err?', err, rowCount)
+    const request = new sql.Request()
+    return request.bulk(table)
+    .then((rowCount) => {
+      console.log(`done exporting ${rowCount} transactions`)
+    })
   })
 }
 
@@ -120,8 +133,10 @@ function exportCheckItems(transaction) {
   })
 
   var request = new sql.Request()
-  request.bulk(table, function(err, rowCount) {
-    console.log('done, err?', err, rowCount)
-  })
+  return request.bulk(table)
+}
 
+function removeTransactions({id: customerId, primaryposref: accountNumber}) {
+  console.log(`removing previous transactions for ${accountNumber} [id:${customerId}]`)
+  return sql.query`delete from transactions where customer_id = ${customerId}`
 }
