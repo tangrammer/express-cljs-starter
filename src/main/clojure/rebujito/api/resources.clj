f75f4d7ce931a513e2c0081de020ad06b4fac84d
Juan A. Ruz <juanantonioruz@gmail.com>
2016-05-31 11:55:18 +0200

    Add else clause to match-exception by status

--- src/main/clojure/rebujito/api/resources.clj
+++ src/main/clojure/rebujito/api/resources.clj
@@ -1,6 +1,6 @@
 (ns rebujito.api.resources
   (:require   [manifold.deferred :as d]
-              [rebujito.api.util :refer (>500* >400*)]))
+              [rebujito.api.util :refer (>500* >400* >base)]))
 
 (defmulti domain-exception "dispatch on data meaning"
   (fn [ctx ex-data] (:type ex-data)))
@@ -8,4 +8,6 @@
 (defmethod domain-exception :api [ctx {:keys [status body]}]
   (condp = status
     400 (>400* ctx body)
-    500 (>500* ctx body)))
+    500 (>500* ctx body)
+    :else (d/error-deferred (ex-info body {:status status}))
+    ))
