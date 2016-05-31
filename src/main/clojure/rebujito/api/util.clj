1a8dfab7327b387b5cd4e225105a6e2bae917410
Juan A. Ruz <juanantonioruz@gmail.com>
2016-06-01 01:25:50 +0200

    Clean logging

--- src/main/clojure/rebujito/api/util.clj
+++ src/main/clojure/rebujito/api/util.clj
@@ -1,10 +1,8 @@
 (ns rebujito.api.util
   (:require [manifold.deferred :as d]
-            [yada.media-type :as mt]
-            [yada.request-body :as rb]
-            [yada.interceptors :as i]
+;            [yada.interceptors :as i]
             [manifold.stream :as stream]
-            [yada.security :as sec]
+ ;           [yada.security :as sec]
             [byte-streams :as bs]
             [yada.handler :as yh]
             [taoensso.timbre :as log]))
@@ -40,67 +38,13 @@
 (defn >200 [ctx body]
   (>base ctx 200 body))
 
-;[b (when (and (= manifold.stream.BufferedStream (type (-> ctx :request :body))) (nil? (-> ctx :parameters :body))) (-> ctx :request :body bs/to-string))]
+(defn log-handler  [ctx]
+  (let [s1 (bs/to-string (-> ctx :response :body ))]
+    (log/info "CALL >>  "  (:response ctx) " :: " (-> ctx :request :uri) " :::: " (-> ctx :parameters) " :: "
+              (when (>= (-> ctx :response :status) 400)
+                s1))
+    (assoc-in ctx [:response :body] (bs/convert s1 java.nio.ByteBuffer))))
 
-(defn log-handler-req  [ctx]
-  (log/info "REQ <<  " (:method ctx) (-> ctx :request :uri) " :::: " (-> ctx :parameters)  )
-  ctx)
-
-(defn log-handler-res  [ctx]
-  (log/info "RES >>  "  (:response ctx) (-> ctx :request :uri)
-            (when (>= (-> ctx :response :status) 400)
-              (-> ctx :response :body bs/to-string)))
-  ctx)
-
-
-
-
-(def default-interceptor-chain
-  [#_(fn [ctx]
-     (clojure.pprint/pprint (:request ctx))
-     (log/info (:request ctx))
-     ctx
-     )
-   i/available?
-   i/known-method?
-   i/uri-too-long?
-   i/TRACE
-
-
-
-   i/method-allowed?
-   i/parse-parameters
-
-   sec/authenticate ; step 1
-
-   i/get-properties ; step 2
-
-
-   sec/authorize ; steps 3,4 and 5
-
-
-
-   i/process-request-body
-
-   log-handler-req
-
-   i/check-modification-time
-   i/select-representation
-   ;; if-match and if-none-match computes the etag of the selected
-   ;; representations, so needs to be run after select-representation
-   ;; - TODO: Specify dependencies as metadata so we can validate any
-   ;; given interceptor chain
-   i/if-match
-   i/if-none-match
-   i/invoke-method
-   i/get-new-properties
-   i/compute-etag
-   sec/access-control-headers
-   #_sec/security-headers
-   i/create-response
-   i/logging
-   i/return
-   ])
 
 (defn common-resource
   ([desc]
@@ -112,9 +56,7 @@
     (common-resource n n)))
   ([desc swagger-tag]
    {:description desc
-;:interceptor-chain (into [log-handler]  (conj yh/default-interceptor-chain log-handler-end) )
-    :interceptor-chain default-interceptor-chain
-    :logger log-handler-res
+    :logger log-handler
     :produces [{:media-type #{"application/json"}
                 :charset "UTF-8"}]
     :swagger/tags (if (vector? swagger-tag)
