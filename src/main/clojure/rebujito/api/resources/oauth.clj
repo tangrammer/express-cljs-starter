2f187c7736b12ff2753f15d19be86f28de1774d7
Juan A. Ruz <juanantonioruz@gmail.com>
2016-06-03 11:28:26 +0200

    Schema for oauth-token works with mobile

--- src/main/clojure/rebujito/api/resources/oauth.clj
+++ src/main/clojure/rebujito/api/resources/oauth.clj
@@ -120,15 +120,13 @@
   (resource
    (-> {:methods
         {:post {:parameters {:query {:sig String}
-                            :body s/Any}
-                           ; TODO: doesn't work with x-www-form-urlencoded
-                            ;  :body (s/conditional
-                            ;         #(= (check-value % :grant_type "client_credentials"))
-                            ;         (-> schema :token-client-credentials)
-                            ;         #(= (check-value % :grant_type "password"))
-                            ;         (-> schema :token-resource-owner)
-                            ;         #(= (check-value % :grant_type "refresh_token"))
-                            ;         (-> schema :token-refresh-token))}
+                             :body (s/conditional
+                                    #(check-value % :grant_type "client_credentials")
+                                    (-> schema :token-client-credentials)
+                                    #(check-value % :grant_type "password")
+                                    (-> schema :token-resource-owner)
+                                    #(check-value % :grant_type "refresh_token")
+                                    (-> schema :token-refresh-token))}
 
                 :consumes [{:media-type #{"application/x-www-form-urlencoded" "application/json"}
                             :charset "UTF-8"}]
