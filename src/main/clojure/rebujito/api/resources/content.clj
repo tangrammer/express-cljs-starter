9013c7fec7b37f293d55142f52470de38a5818ac
Marcin Jekot <marcin@jekot.net>
2016-06-09 21:11:37 +0200

    content/terms is actually meant to be content-type application/json, and we should accept locale in the query string.
    @tangrammer -> this is still not working from the app :( I will open an issue

--- src/main/clojure/rebujito/api/resources/content.clj
+++ src/main/clojure/rebujito/api/resources/content.clj
@@ -1,12 +1,13 @@
 (ns rebujito.api.resources.content
   (:require
+   [schema.core :as s]
    [rebujito.api.util :as util]))
 
 (defn terms []
   (->
    {:methods
-    {:get {:parameters {:query {:access_token String}
-                        :path {:market String}}
+    {:get {:parameters {:query {:access_token String
+                                (s/optional-key :locale) String}}
            :response (slurp (clojure.java.io/resource "mocks/lorem_ipsum.html"))}}}
    (merge (util/common-resource :content/terms)
-          {:produces "text/html"})))
+          {:produces "application/json"})))
