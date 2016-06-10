8e6d394fe3f4f7a3124d3f58557a7a15b665e6b5
Marcin Jekot <marcin@jekot.net>
2016-06-10 12:00:33 +0200

    formatting of terms

--- src/main/clojure/rebujito/api/resources/content.clj
+++ src/main/clojure/rebujito/api/resources/content.clj
@@ -1,17 +1,15 @@
 (ns rebujito.api.resources.content)
 
 (def terms-json {:name "Terms of Use"
-              	 :item {
-              		 :id nil
-              		 :templateId nil
-              		 :templateName "Mobile Content"
-              		 :displayname "Terms of Use"
-              		 :headline ""
-              		 :image ""
-              		 :imagehires ""
-              		 :key ""
-              		 :sharetext ""
-              		 :text (slurp (clojure.java.io/resource "mocks/lorem_ipsum.html"))
-              		 :updated nil
-              	 }
+              	 :item {:id nil
+                    		:templateId nil
+                    		:templateName "Mobile Content"
+                    		:displayname "Terms of Use"
+                    		:headline ""
+                    		:image ""
+                    		:imagehires ""
+                    		:key ""
+                    		:sharetext ""
+                    		:text (slurp (clojure.java.io/resource "mocks/lorem_ipsum.html"))
+                    		:updated nil}
               	 :children []})
