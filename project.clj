ccd98641398c11731bfed4e58c98e4c70eeec6e5
Marcin Jekot <marcin@jekot.net>
2016-06-12 13:46:29 +0200

    bump yada version (and prismatic/schema + metosin/ring-swagger along with it)

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [aleph "0.4.2-alpha4" :exclusions [org.clojure/clojure]]
                  [bidi "2.0.6" :exclusions [ring/ring-core]]
                  [ring/ring-core "1.4.0"]
-                 [yada "1.1.6" :exclusions [bidi org.clojure/clojurescript buddy/buddy-sign]]
+                 [yada "1.1.18" :exclusions [bidi org.clojure/clojurescript buddy/buddy-sign]]
 
                  ;; optional http client
                  ;; https://www.lvh.io/posts/https-requests-with-client-certificates-in-clojure.html
@@ -47,11 +47,11 @@
 
                  [potemkin "0.4.3" :exclusions [riddley]]
                  [prismatic/plumbing "0.5.3" :exclusions [prismatic/schema]]
-                 [prismatic/schema "1.1.0" :exclusions [potemkin]]
+                 [prismatic/schema "1.1.1" :exclusions [potemkin]]
                  [prismatic/schema-generators "0.1.0"]
 
                  ;; swagger-ui
-                 [metosin/ring-swagger "0.22.6" :exclusions [potemkin]]
+                 [metosin/ring-swagger "0.22.7" :exclusions [potemkin]]
                  [metosin/ring-swagger-ui "2.1.4-0"]
                  [org.webjars/swagger-ui "2.1.4"]
                  [org.webjars/jquery "2.1.4"]
