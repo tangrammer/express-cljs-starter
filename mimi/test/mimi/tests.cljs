bd8edccdf90dc8590d4d4b6b0f2b5a54f527652e
Marcin Jekot <marcin@jekot.net>
2016-07-21 12:52:16 +0200

    mimi: add tests for validate-link-card-data-test (only a string should pass)

--- mimi/test/mimi/tests.cljs
+++ mimi/test/mimi/tests.cljs
@@ -1,6 +1,6 @@
 (ns mimi.tests
   (:require
-    [mimi.data :refer [validate-create-customer-data]]
+    [mimi.data :refer [validate-create-customer-data validate-link-card-data]]
     [cljs.test :refer-macros [deftest is testing run-tests]]
     [cljs.nodejs :as nodejs]))
 
@@ -20,6 +20,12 @@
   (is (not (nil? (validate-create-customer-data (assoc valid-create-customer-data :extra :field)))))
   (is (not (nil? (validate-create-customer-data (dissoc valid-create-customer-data :email)))))
   (is (not (nil? (validate-create-customer-data (assoc (dissoc valid-create-customer-data :email) :extra :field))))))
+
+(deftest validate-link-card-data-test
+  (is (not (nil? (validate-link-card-data ""))))
+  (is (not (nil? (validate-link-card-data {:cardNumber 9623570901113, :customerId "44051647"}))))
+  (is (nil? (validate-link-card-data {:cardNumber "9623570901113", :customerId "44051647"})))
+  )
 
 (defn run []
   (nodejs/enable-util-print!)
