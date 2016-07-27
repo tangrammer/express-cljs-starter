51b78928ab0df03afaf2aac78ded4f9cfc3e8bc3
Marcin Jekot <marcin@jekot.net>
2016-07-27 08:06:58 +0200

    fnb payment play area

---
+++ src/main/clojure/payment_fnb/core.clj
@@ -0,0 +1,9 @@
+(ns payment-fnb.core
+  (:require
+    [clj-http.client
+     ]))
+
+(def base-uri "https://sandbox.ms.fnb.co.za/mtokenizer/api/instance")
+
+(def api-key "ChDQ8bAKeOjlZBBfez61BQSG5f8Ko88dOacNMIDVQfhfThxInpwwa5XNBrD2WzyT")
+
